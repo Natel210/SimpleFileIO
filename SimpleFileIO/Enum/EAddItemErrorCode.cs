e86f5d012289ba0c61c6ec7124643c60d6ef61cf
Natel <51104656+Natel210@users.noreply.github.com>
2024-11-21 11:05:32 +0900

    init

---
+++ SimpleFileIO/Enum/EAddItemErrorCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFileIO.Enum
+{
+    public enum EAddItemErrorCode
+    {
+        Unknown = -1,
+        OK,
+        WorkException,
+        DifferentDataType,
+    }
+}
