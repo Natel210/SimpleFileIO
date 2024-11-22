88dc78ed345a8b8246c8edb19efdb18a4dc50046
Natel <51104656+Natel210@users.noreply.github.com>
2024-11-22 09:00:50 +0900

    Update EWriteItemErrorCode.cs

--- SimpleFileIO/Enum/EWriteItemErrorCode.cs
+++ SimpleFileIO/Enum/EWriteItemErrorCode.cs
@@ -17,5 +17,6 @@ namespace SimpleFileIO.Enum
         TaskRunWritingException, // An error occurred while writing to the actual file.
         TaskRunCancellationTokenException, //Problem with refraining from tokens and task variables during task run operation.
         TaskRunInvalidRootPath, // not root path null
+
     }
 }
