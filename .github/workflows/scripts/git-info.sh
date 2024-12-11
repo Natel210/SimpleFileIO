b0cf0814582719988ae88003bfe03bc1381f9178
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-11 15:50:19 +0900

    updateTest

--- .github/workflows/scripts/git-info.sh
+++ .github/workflows/scripts/git-info.sh
@@ -1,6 +1,5 @@
 if [[ "${{ github.event_name }}" == "push" ]]; then
     echo "Event: Push"
-
     # Push Event: Extract Branch or Tag Information
     ref=${{ github.ref }}
     if [[ "$ref" == refs/heads/* ]]; then
@@ -13,12 +12,10 @@ if [[ "${{ github.event_name }}" == "push" ]]; then
         echo "Unknown ref type for push: $ref"
         exit 1
     fi
-
     # Commit Information
     commit_hash=$(git rev-parse HEAD)
     commit_message=$(git log -1 --pretty=format:%s)
     commit_author=$(git log -1 --pretty=format:'%an <%ae>')
-
     # Output
     echo "Branch Name      : $branch_name"
     echo "Tag Name         : $tag_name"
@@ -28,16 +25,13 @@ if [[ "${{ github.event_name }}" == "push" ]]; then
 
 elif [[ "${{ github.event_name }}" == "pull_request" ]]; then
   echo "Event: Pull Request"
-          
   # Pull Request Event: Extract Source and Target Branch Information
   source_branch=${{ github.head_ref }}
   target_branch=${{ github.base_ref }}
-          
   # Commit Information
   commit_hash=$(git rev-parse HEAD)
   commit_message=$(git log -1 --pretty=format:%s)
   commit_author=$(git log -1 --pretty=format:'%an <%ae>')
-          
   # Output
   echo "Source Branch Name : $source_branch"
   echo "Target Branch Name : $target_branch"
@@ -48,5 +42,5 @@ elif [[ "${{ github.event_name }}" == "pull_request" ]]; then
 else
   echo "Unknown event type: ${{ github.event_name }}"
   exit 1
-  
+
 fi
