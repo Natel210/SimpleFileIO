836d7a20aeb9f52a95fee9838ea5f19696f5c4c7
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-12 16:24:23 +0900

    Update git-info.sh

--- .github/actions/info/git/scripts/git-info.sh
+++ .github/actions/info/git/scripts/git-info.sh
@@ -9,30 +9,29 @@ head_ref="${4:-unknown_head_ref}"
 base_ref="${5:-unknown_base_ref}"
 
 echo "::group::Git Info ($event_name)"
-echo "Git Info"
-echo "|- Script Directory"
-echo "|   |- $scripts_path"
-echo "|- Git Info Script"
-echo "|   |- Event Name: $event_name"
-echo "|   |- Ref: $ref"
-echo "|   |- Head Ref: $head_ref"
-echo "|   |- Base Ref: $base_ref"
+echo "    Script Directory : $scripts_path"
+echo "    ::group::Get Info"
+echo "        Event Name: $event_name"
+echo "        Ref: $ref"
+echo "        Head Ref: $head_ref"
+echo "        Base Ref: $base_ref"
+echo "    ::endgroup::"
 
 # Handle different events
 case "$event_name" in
   push)
-    echo "::group::Push Info (Branch : $ref)"
+    echo "    ::group::Push Info (Branch : $ref)"
     "$scripts_path/process-push.sh" "$ref"
     echo "|   |"
-    echo "::endgroup::"
-    echo "::endgroup::"
+    echo "    ::endgroup::"
+    echo "    ::endgroup::"
     ;;
   pull_request)
-    echo "::group::Pull Request Info (source : $head_ref, target : $base_ref)"
+    echo "    ::group::Pull Request Info (source : $head_ref, target : $base_ref)"
     "$scripts_path/process-pull-request.sh" "$head_ref" "$base_ref"
     echo "================================================================================"
-    echo "::endgroup::"
-    echo "::endgroup::"
+    echo "    ::endgroup::"
+    echo "    ::endgroup::"
     ;;
   *)
     echo "|- Unknown event type: $event_name"
