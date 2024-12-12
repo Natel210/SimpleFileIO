bc9567719ac1df98002f72adb1bb513bff4198e5
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-12 17:54:27 +0900

    Update process-push.sh

--- .github/actions/info/git/scripts/process-push.sh
+++ .github/actions/info/git/scripts/process-push.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
+# ANSI Color Codes
 BOLD="\033[1m"
-YELLOW="\033[33m"
+CYAN="\033[36m"
+GREEN="\033[32m"
+RED="\033[31m"
 RESET="\033[0m"
 
 ref="${1:-unknown_ref}"
 
 if [[ "$ref" == refs/heads/* ]]; then
   branch_name=${ref#refs/heads/}
-  echo "> Push to branch : \033[1m\033[36m$branch_name\033[0m"
+  echo "> Push to branch : ${BOLD}${CYAN}$branch_name${RESET}"
   echo "::group::"
   echo "  > Ref: $ref"
   echo "  > Branch Name: $branch_name"
   echo "::endgroup::"
 
-  echo "> Push to tag : \033[1m\033[32m$tag_name\033[0m"
+  echo "> Push to tag : ${BOLD}${GREEN}$tag_name${RESET}"
   echo "::group::"
   echo "  > Ref: $ref"
   echo "  > Tag Name: $tag_name"
   echo "::endgroup::"
 
-    echo "> \033[1m\033[31m Unknown : $ref\033[0m"
+    echo "> ${BOLD}${RED}Unknown : $ref${RESET}"
   
 elif [[ "$ref" == refs/tags/* ]]; then
   tag_name=${ref#refs/tags/}
