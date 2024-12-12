b00da31c17167229d08b034890417c63f7ce8dec
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-12 17:43:09 +0900

    Update process-push.sh

--- .github/actions/info/git/scripts/process-push.sh
+++ .github/actions/info/git/scripts/process-push.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+BOLD="\033[1m"
+YELLOW="\033[33m"
+RESET="\033[0m"
+
 ref="${1:-unknown_ref}"
 
 if [[ "$ref" == refs/heads/* ]]; then
   branch_name=${ref#refs/heads/}
-  echo "::group::Push Info (branch : \033[1m\033[32m$branch_name\033[0m)"
+  echo "::group::Push Info (branch : ${BOLD}${YELLOW}$branch_name${RESET})"
   echo "  > Ref: $ref"
   echo "  > Branch Name: $branch_name"
   echo "::endgroup::"
   
 elif [[ "$ref" == refs/tags/* ]]; then
   tag_name=${ref#refs/tags/}
-  echo "::group::Push Info (tag : \033[1m\033[32m$tag_name\033[0m)"
+  echo "::group::Push Info (tag : ${BOLD}${YELLOW}$tag_name${RESET})"
   echo "  > Ref: $ref"
   echo "  > Tag Name: $tag_name"
   echo "::endgroup::"
 else
-  echo "::group::Push Info (\033[1m\033[31mUnknown\033[0m)"
+  echo "::group::Push Info (${BOLD}${YELLOW}Unknown${RESET})"
   echo "  > Ref: $ref"
   echo "::endgroup::"
   exit 1
