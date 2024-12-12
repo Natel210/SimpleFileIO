9a74b8b4773364b86f1825034b9e2a512672d3de
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-12 15:55:51 +0900

    test

--- .github/actions/info/git/scripts/git-info.sh
+++ .github/actions/info/git/scripts/git-info.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-# Read arguments
-event_name="${1:-unknown_event}"
-ref="${2:-unknown_ref}"
-head_ref="${3:-unknown_head_ref}"
-base_ref="${4:-unknown_base_ref}"
 
-echo "Git Info Script"
-echo "Event Name: $event_name"
-echo "Ref: $ref"
-echo "Head Ref: $head_ref"
-echo "Base Ref: $base_ref"
+# Read arguments
+scripts_path="${1}"
+event_name="${2}"
+ref="${3:-unknown_ref}"
+head_ref="${4:-unknown_head_ref}"
+base_ref="${5:-unknown_base_ref}"
+
+echo "Git Info"
+echo "|- Script Directory"
+echo "|   |- $scripts_path"
+echo "|- Git Info Script"
+echo "|   |- Event Name: $event_name"
+echo "|   |- Ref: $ref"
+echo "|   |- Head Ref: $head_ref"
+echo "|   |- Base Ref: $base_ref"
 
 # Handle different events
 case "$event_name" in
   push)
-    echo "Handling Push Event"
-    .github/scripts/info/git/process-push.sh "$ref"
+    echo "|- Push"
+    "$scripts_path/process-push.sh" "$ref"
     ;;
   pull_request)
-    echo "Handling Pull Request Event"
-    .github/scripts/info/git/process-pull-request.sh "$head_ref" "$base_ref"
+    echo "|- Pull Request"
+    "$scripts_path/process-pull-request.sh" "$head_ref" "$base_ref"
     ;;
   *)
-    echo "Unknown event type: $event_name"
+    echo "|- Unknown event type: $event_name"
     exit 1
     ;;
 esac
