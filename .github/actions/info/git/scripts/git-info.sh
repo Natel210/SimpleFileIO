45fe0a68d60c8d51930c82126f580053a7ee1143
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-12 16:07:07 +0900

    test

--- .github/actions/info/git/scripts/git-info.sh
+++ .github/actions/info/git/scripts/git-info.sh
@@ -7,8 +7,8 @@ event_name="${2}"
 ref="${3:-unknown_ref}"
 head_ref="${4:-unknown_head_ref}"
 base_ref="${5:-unknown_base_ref}"
-
-echo "Git Info"
+echo "----------------------------------------"
+echo "** Git Info **"
 echo "|- Script Directory"
 echo "|   |- $scripts_path"
 echo "|- Git Info Script"
@@ -22,13 +22,16 @@ case "$event_name" in
   push)
     echo "|- Push"
     "$scripts_path/process-push.sh" "$ref"
+    echo "----------------------------------------"
     ;;
   pull_request)
     echo "|- Pull Request"
     "$scripts_path/process-pull-request.sh" "$head_ref" "$base_ref"
+    echo "----------------------------------------"
     ;;
   *)
     echo "|- Unknown event type: $event_name"
+    echo "----------------------------------------"
     exit 1
     ;;
 esac
