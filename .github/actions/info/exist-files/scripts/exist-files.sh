74a1b46c036a2e667660c7cc2a69514189581bbc
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-13 13:26:56 +0900

    Update exist-files.sh

--- .github/actions/info/exist-files/scripts/exist-files.sh
+++ .github/actions/info/exist-files/scripts/exist-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Check if at least one argument is provided
 if [ "$#" -lt 1 ]; then
-  echo "::error::No arguments provided. \n Usage: $0 <file1> [file2] [file3] ..."
+  echo "::error::No arguments. \n Usage: $0 <file1> [file2] [file3] ..."
   exit 1
 fi
 
@@ -17,15 +16,15 @@ done
 if [ $is_miss_files != 0 ]; then
   echo -e "::warning::One or more files are missing."
 else
-  echo -e "::notice::\033[32mAll files are present.\033[0m"
+  echo -e "::notice::\[\033[0;32m\]All files Exist.\[\033[0m\]"
 fi
 
-echo -e "::group::Check Files List"
+echo -e "::group::Files List"
 for file in "$@"; do
   if [ -f "$file" ]; then
-    echo "> $file : Exist"
+    echo -e "> $file : \[\033[0;34m\]Exist\[\033[0m\]"
   else
-    echo -e "> $file : \033[31mNot Exist\033[0m"
+    echo -e "> $file : \[\033[0;31m\]Not Exist\[\033[0m\]"
   fi
 done
 echo "::endgroup::"
