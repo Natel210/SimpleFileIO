8ca804e91ecb90975d355b1b91da8da2a653d6d0
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-13 12:44:20 +0900

    test

--- .github/actions/info/git/scripts/git-info-push.sh
+++ .github/actions/info/git/scripts/git-info-push.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
+
 ref="${1:-unknown_ref}"
 
 if [[ "$ref" == refs/heads/* ]]; then
   branch_name=${ref#refs/heads/}
-  echo "::group::  - Info (Push - branch : $branch_name)"
+  echo -e "::group::  - Info (\033[32mPush\033[0m - branch : \033[34m$branch_name\033[0m)"
   echo "    > Ref: $ref"
-  echo "    > Branch Name: $branch_name"
+  echo -e "    > Branch Name: \033[34m$branch_name\033[0m"
   echo "::endgroup::"
-  echo "::info::Push Branch: $branch_name"
-
+  
 elif [[ "$ref" == refs/tags/* ]]; then
   tag_name=${ref#refs/tags/}
-  echo "::group::  - Info (Push - tag : $tag_name)"
+  echo -e "::group::  - Info (\033[32mPush\033[0m - tag : \033[34m$tag_name\033[0m)"
   echo "    > Ref: $ref"
-  echo "    > Tag Name: $tag_name"
+  echo -e "    > Tag Name: \033[34m$tag_name\033[0m"
   echo "::endgroup::"
-  echo "::info::Push Tag: $tag_name"
-
 else
-  echo "::group::  - Info (Push - Unknown)"
+  echo -e "::group::  - Info (\033[32mPush\033[0m - \033[31mUnknown\033[0m)"
   echo "    > Ref: $ref"
   echo "::endgroup::"
-  echo "::error::Unknown push reference: $ref"
   exit 1
-
 fi
--- .github/actions/info/git/scripts/git-info.sh
+++ .github/actions/info/git/scripts/git-info.sh
@@ -8,10 +8,10 @@ ref="${3:-unknown_ref}"
 head_ref="${4:-unknown_head_ref}"
 base_ref="${5:-unknown_base_ref}"
 
-echo "⤷ - Git Info"
-echo "::group::  - Arguments"
+echo -e "⤷ - \033[32mGit Info\033[0m -"
+echo -e "::group::  - Arguments"
 echo "    > Script Directory : $scripts_path"
-echo "    > Event Name: $event_name"
+echo -e "    > Event Name: \033[32m$event_name\033[0m"
 echo "    > Ref: $ref"
 echo "    > Head Ref: $head_ref"
 echo "    > Base Ref: $base_ref"
@@ -20,13 +20,13 @@ echo "::endgroup::"
 # Handle different events
 case "$event_name" in
   push)
-    "$scripts_path/git-info-push.sh" "$ref"
+    "$scripts_path/process-push.sh" "$ref"
     ;;
   pull_request)
-    "$scripts_path/git-info-pull-request.sh" "$head_ref" "$base_ref"
+    "$scripts_path/process-pull-request.sh" "$head_ref" "$base_ref"
     ;;
   *)
-    echo "⤷ Unknown event type: $event_name"
+    echo -e "::error::⤷ \033[31mUnknown event type: $event_name\033[0m"
     echo " "
     exit 1
     ;;
