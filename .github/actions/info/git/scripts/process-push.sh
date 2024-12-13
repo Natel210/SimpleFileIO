bd8039ef87703f29751fb9a5741f328e4d695902
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-13 09:14:00 +0900

    test

--- .github/actions/info/git/scripts/process-push.sh
+++ .github/actions/info/git/scripts/process-push.sh
@@ -5,19 +5,19 @@ ref="${1:-unknown_ref}"
 
 if [[ "$ref" == refs/heads/* ]]; then
   branch_name=${ref#refs/heads/}
-  echo -e "::group::Info (branch : \033[34m$branch_name\033[0m)"
+  echo -e "::group::Info (\033[32mPush\033[0m branch : \033[34m$branch_name\033[0m)"
   echo "> Ref: $ref"
   echo -e "> Branch Name: \033[34m$branch_name\033[0m"
   echo "::endgroup::"
   
 elif [[ "$ref" == refs/tags/* ]]; then
   tag_name=${ref#refs/tags/}
-  echo -e "::group::Info (tag : \033[34m$tag_name\033[0m)"
+  echo -e "::group::Info (\033[32mPush\033[0m tag : \033[34m$tag_name\033[0m)"
   echo "> Ref: $ref"
   echo -e "> Tag Name: \033[34m$tag_name\033[0m"
   echo "::endgroup::"
 else
-  echo -e "::group::Info (\033[31mUnknown\033[0m)"
+  echo -e "::group::Info (\033[32mPush\033[0m \033[31mUnknown\033[0m)"
   echo "> Ref: $ref"
   echo "::endgroup::"
   exit 1
