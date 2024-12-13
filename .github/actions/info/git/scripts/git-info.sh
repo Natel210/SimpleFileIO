8048adf3d34752013c759a5a14f3527ed3a86190
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-13 12:55:08 +0900

    test

--- .github/actions/info/git/scripts/git-info.sh
+++ .github/actions/info/git/scripts/git-info.sh
@@ -8,13 +8,12 @@ ref="${3:-unknown_ref}"
 head_ref="${4:-unknown_head_ref}"
 base_ref="${5:-unknown_base_ref}"
 
-echo -e "⤷ - \033[32mGit Info\033[0m -"
-echo -e "::group::  - Arguments"
-echo "    > Script Directory : $scripts_path"
-echo -e "    > Event Name: \033[32m$event_name\033[0m"
-echo "    > Ref: $ref"
-echo "    > Head Ref: $head_ref"
-echo "    > Base Ref: $base_ref"
+echo -e "::group::Arguments"
+echo "> Script Directory : $scripts_path"
+echo -e "> Event Name: \033[32m$event_name\033[0m"
+echo "> Ref: $ref"
+echo "> Head Ref: $head_ref"
+echo "> Base Ref: $base_ref"
 echo "::endgroup::"
 
 # Handle different events
@@ -26,7 +25,7 @@ case "$event_name" in
     "$scripts_path/git-info-pull-request.sh" "$head_ref" "$base_ref"
     ;;
   *)
-    echo -e "::error::⤷ \033[31mUnknown event type: $event_name\033[0m"
+    echo -e "::error::\033[31mUnknown event type: $event_name\033[0m"
     echo " "
     exit 1
     ;;
