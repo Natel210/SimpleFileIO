d6ecec16fba1ef7d8ed27fc3ccaa89f5d0d1c9ab
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-13 11:53:05 +0900

    test

--- .github/actions/info/env-check/scripts/env-check.sh
+++ .github/actions/info/env-check/scripts/env-check.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-event_name="${1:-unknown_event}"
-ref="${2:-unknown_ref}"
-head_ref="${3:-unknown_head_ref}"
-base_ref="${4:-unknown_base_ref}"
-solution_file="${5:-unknown_solution_file}"
-dll_file="${6:-unknown_dll_file}"
-tester_file="${7:-unknown_tester_file}"
+# Load the JSON config
+CONFIG="$1"
 
+# Start GitHub Actions log group
+echo "::group::Env Values"
 
-echo "Setting environment variables for Git Info Script."
-echo "EVNET: $event_name"
-echo "REF: $ref"
-echo "HEAD_REF: $head_ref"
-echo "BASE_REF: $base_ref"
-echo "SOLUTION_FILE: $solution_file"
-echo "DLL_FILE: $dll_file"
-echo "TESTER_FILE: $tester_file"
+# Process JSON and format output
+echo "$CONFIG" | jq -r '. | to_entries[] | "> [\(.key)] : \(.value)"'
+
+# End GitHub Actions log group
+echo "::endgroup::"
