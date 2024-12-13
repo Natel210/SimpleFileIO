c8754e0069752ad8e7f2ff9fb0825dbafe41fc6d
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-13 12:58:45 +0900

    test

--- .github/actions/info/git/scripts/git-info.sh
+++ .github/actions/info/git/scripts/git-info.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Read arguments
 scripts_path="${1}"
 event_name="${2}"
@@ -8,13 +7,7 @@ ref="${3:-unknown_ref}"
 head_ref="${4:-unknown_head_ref}"
 base_ref="${5:-unknown_base_ref}"
 
-echo -e "::group::Arguments"
-echo "> Script Directory : $scripts_path"
-echo -e "> Event Name: \033[32m$event_name\033[0m"
-echo "> Ref: $ref"
-echo "> Head Ref: $head_ref"
-echo "> Base Ref: $base_ref"
-echo "::endgroup::"
+is_error=0
 
 # Handle different events
 case "$event_name" in
@@ -26,8 +19,18 @@ case "$event_name" in
     ;;
   *)
     echo -e "::error::\033[31mUnknown event type: $event_name\033[0m"
-    echo " "
-    exit 1
+    is_error=1
     ;;
 esac
-echo "---- ----"
+
+echo "::group::Arguments"
+echo "> Script Directory : $scripts_path"
+echo "> Event Name: $event_name"
+echo "> Ref: $ref"
+echo "> Head Ref: $head_ref"
+echo "> Base Ref: $base_ref"
+echo "::endgroup::"
+
+if [[ "$is_error" != 0 ]]; then
+    exit 1
+fi
