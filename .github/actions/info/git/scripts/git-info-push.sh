3f2b934096c3fb73c78b51f1502e56af07d3b206
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-13 12:33:19 +0900

    Update git-info-push.sh

--- .github/actions/info/git/scripts/git-info-push.sh
+++ .github/actions/info/git/scripts/git-info-push.sh
@@ -4,22 +4,25 @@ ref="${1:-unknown_ref}"
 
 if [[ "$ref" == refs/heads/* ]]; then
   branch_name=${ref#refs/heads/}
-  echo -e "::group::  - Info (\033[32mPush\033[0m - branch : \033[34m$branch_name\033[0m)"
+  echo "::group::  - Info (Push - branch : $branch_name)"
   echo "    > Ref: $ref"
-  echo -e "    > Branch Name: \033[34m$branch_name\033[0m"
+  echo "    > Branch Name: $branch_name"
   echo "::endgroup::"
   echo "::info::Push Branch: $branch_name"
+
 elif [[ "$ref" == refs/tags/* ]]; then
   tag_name=${ref#refs/tags/}
-  echo -e "::group::  - Info (\033[32mPush\033[0m - tag : \033[34m$tag_name\033[0m)"
+  echo "::group::  - Info (Push - tag : $tag_name)"
   echo "    > Ref: $ref"
-  echo -e "    > Tag Name: \033[34m$tag_name\033[0m"
+  echo "    > Tag Name: $tag_name"
   echo "::endgroup::"
   echo "::info::Push Tag: $tag_name"
+
 else
-  echo -e "::group::  - Info (\033[32mPush\033[0m - \033[31mUnknown\033[0m)"
+  echo "::group::  - Info (Push - Unknown)"
   echo "    > Ref: $ref"
   echo "::endgroup::"
   echo "::error::Unknown push reference: $ref"
   exit 1
+
 fi
