e4b3e5433e7c5949864134d8fd7a5f2841922e5b
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-18 16:41:15 +0900

    Update values.sh

--- .scripts/check/values.sh
+++ .scripts/check/values.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 
-
-
 JSON_FILE="$1"
+RESULT_FILE="$2"
 
 while IFS="=" read -r key value; do
   echo "${key} : ${value}"
 done < <(cat $JSON_FILE | jq -r 'to_entries[] | "\(.key)=\(.value)"')
-
-
