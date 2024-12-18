81d9ac12461c8143232c65cf0ec6553cdeb67563
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-18 17:00:13 +0900

    test

--- .scripts/check/values.sh
+++ .scripts/check/values.sh
@@ -4,12 +4,10 @@
 JSON_FILE="$1"
 RESULT_FILE="$2"
 
-# JSON 데이터에서 키와 값을 파싱하여 출력
 if [ -z "$RESULT_FILE" ]; then
-  # RESULT_FILE이 없으면 표준 출력으로 출력
   echo "$JSON_DATA" | jq -r 'to_entries[] | "\(.key) : \(.value)"'
 else
-  # RESULT_FILE이 있으면 파일로 저장
+  touch $RESULT_FILE
   echo "$JSON_DATA" | jq -r 'to_entries[] | "\(.key) : \(.value)"' > "$RESULT_FILE"
   cat "$RESULT_FILE"
 fi
