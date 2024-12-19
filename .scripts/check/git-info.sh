2aa9dbd4df169efa0559d57530609cd34ec71910
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-19 10:13:10 +0900

    Update git-info.sh

--- .scripts/check/git-info.sh
+++ .scripts/check/git-info.sh
@@ -18,18 +18,18 @@ is_error=0
 case "$event_name" in
   push)
     output="● Detail"
-    output+="  - Event : \033[32mPush\033[0m\n"
-    output+="  - Ref: $ref\n"
+    output+="\n  - Event : \033[32mPush\033[0m"
+    output+="\n  - Ref: $ref"
     if [[ "$ref" == refs/heads/* ]]; then
       branch_name=${ref#refs/heads/}
-      output+="  - Branch: \033[34m$branch_name\033[0m\n"
+      output+="\n  - Branch: \033[34m$branch_name\033[0m"
       summary="\033[32mPush\033[0m Branch : \033[34m$branch_name\033[0m\n"
     elif [[ "$ref" == refs/tags/* ]]; then
       tag_name=${ref#refs/tags/}
-      output+="  - Branch: \033[34m$tag_name\033[0m\n"
+      output+="\n  - Branch: \033[34m$tag_name\033[0m"
       summary="\033[32mPush\033[0m Tag : \033[34m$tag_name\033[0m\n"
     else
-      output+="  - \033[31mUnknown\033[0m\n"
+      output+="\n  - \033[31mUnknown\033[0m"
       summary="\033[32mPush\033[0m \033[31mUnknown\033[0m\n"
       is_error=1
     fi
@@ -37,28 +37,28 @@ case "$event_name" in
   pull_request)
     summary="\033[32mPull Request\033[0m"
     output="● Detail"
-    output+="  - Event : \033[32mmPull Request\033[0m\n"
+    output+="\n  - Event : \033[32mmPull Request\033[0m\n"
     if [[ "$head_ref" == refs/heads/* ]]; then
-      output+="  - Head Ref : \033[31m$head_ref\033[0m"
+      output+="\n  - Head Ref : \033[31m$head_ref\033[0m"
       summary+="\n  \033[31mInvalid Head Ref ($head_ref) \033[0m"
       is_error=1
     else
-      output+="  - Head Ref : $head_ref"
+      output+="\n  - Head Ref : $head_ref"
     fi
     if [[ "$base_ref" == refs/heads/* ]]; then
-      output+="  - Base Ref : \033[31m$base_ref\033[0m"
+      output+="\n  - Base Ref : \033[31m$base_ref\033[0m"
       summary+="\n  \033[31mInvalid Base Ref ($base_ref) \033[0m"
       is_error=1
     else
-      output+="  - Base Ref : $base_ref"
+      output+="\n  - Base Ref : $base_ref"
     fi
     
     if [[ "$is_error" == "0" ]]; then
       head_branch_name=${head_ref#refs/heads/}
       base_branch_name=${base_ref#refs/heads/}
-      output+="  - Head Branch : $head_branch_name"
-      output+="  - Base Branch : $base_branch_name"
-      output+="  $head_branch_name -> $base_branch_name"
+      output+="\n  - Head Branch : $head_branch_name"
+      output+="\n  - Base Branch : $base_branch_name"
+      output+="\n  $head_branch_name -> $base_branch_name"
       summary+=" : $head_branch_name -> $base_branch_name"
     fi
     summary+="\n"
