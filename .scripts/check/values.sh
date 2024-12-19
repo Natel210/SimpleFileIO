1b7540205ebe5620abb85566144a3e6f7bd262a4
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-19 11:40:50 +0900

    test

--- .scripts/check/values.sh
+++ .scripts/check/values.sh
@@ -11,9 +11,9 @@ result_file="$2"
 
 
 if [ -z "$result_file" ]; then
-  echo "$json_data" | jq -r 'to_entries[] | "\(.key) : \(.value)"'
+  echo -e "$json_data" | jq -r 'to_entries[] | "\(.key) : \(.value)"'
 else
   touch $result_file
-  echo "$json_data" | jq -r 'to_entries[] | "\(.key) : \(.value)"' > "$result_file"
+  echo -e "$json_data" | jq -r 'to_entries[] | "\(.key) : \(.value)"' > "$result_file"
   cat "$result_file"
 fi
