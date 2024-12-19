e00d33a3e9cefe2bcff3a23d0d30a76abb74000b
Natel <51104656+Natel210@users.noreply.github.com>
2024-12-19 10:58:27 +0900

    test

--- .scripts/check/exist-files.sh
+++ .scripts/check/exist-files.sh
@@ -3,13 +3,20 @@
 json_data="$1"
 result_file="$2"
 
-TEXT_BLACK="\033[38;5;0m"
+TEXT_WHITE="\033[38;5;15m"
 TEXT_RED="\033[38;5;196m"
+TEXT_BLUE="\033[38;5;21m"
 TEXT_GREEN="\033[38;5;46m"
-TEXT_BLUE="\033[38;5;51m"
-BACKGROUND_DARK_GRAY="\033[48;5;235m"
-BACKGROUND_DARK_GREEN="\033[48;5;22m"
+TEXT_LIGHT_GRAY="\033[38;5;250m"
+TEXT_DARK_RED="\033[38;5;124m"
+TEXT_DARK_BLUE="\033[38;5;20m"
+TEXT_DARK_GREEN="\033[38;5;28m"
+
+BACKGROUND_LIGHT_GRAY="\033[48;5;245m"
 BACKGROUND_DARK_RED="\033[48;5;52m"
+BACKGROUND_DARK_BLUE="\033[48;5;19m"
+BACKGROUND_DARK_GREEN="\033[48;5;22m"
+
 RESET="\033[0m"
 
 if [ -z "$json_data" ]; then
@@ -31,17 +38,17 @@ output=""
 while IFS=$'\t' read -r key path; do
   total_count=$((total_count + 1))
   if [ -f "$path" ] || [ -d "$path" ]; then
-    output+="\n● $key\n  - $path : ${TEXT_BLUE}Exist${RESET}"
+    output+="\n● $key\n  - $path : ${TEXT_DARK_BLUE}Exist${RESET}"
   else
     missing_count=$((missing_count + 1))
-    output+="\n● $key\n  - $path : ${TEXT_RED}Not Exist${RESET}"
+    output+="\n● $key\n  - $path : ${TEXT_DARK_RED}Not Exist${RESET}"
   fi
 done <<< "$FILES"
 
 if [ "$missing_count" -eq 0 ]; then
-  summary="${BACKGROUND_DARK_GREEN}${TEXT_BLACK}All OK.. ($total_count/$total_count)${RESET}\n"
+  summary="${BACKGROUND_DARK_GREEN}${TEXT_GREEN}All OK.. ($total_count/$total_count)${RESET}\n"
 else
-  summary="${BACKGROUND_DARK_RED}${TEXT_BLACK}Not Exist File Count $missing_count.. ($((total_count - missing_count))/$total_count)${RESET}\n"
+  summary="${BACKGROUND_DARK_RED}${TEXT_RED}Not Exist File Count $missing_count.. ($((total_count - missing_count))/$total_count)${RESET}\n"
 fi
 
 result="$summary$output"
--- .scripts/check/git-info.sh
+++ .scripts/check/git-info.sh
@@ -13,68 +13,78 @@ head_ref="${3:-unknown_head_ref}"
 base_ref="${4:-unknown_base_ref}"
 result_file="$5"
 
-TEXT_BLACK="\033[38;5;0m"
+TEXT_WHITE="\033[38;5;15m"
 TEXT_RED="\033[38;5;196m"
+TEXT_BLUE="\033[38;5;21m"
 TEXT_GREEN="\033[38;5;46m"
-TEXT_BLUE="\033[38;5;51m"
-BACKGROUND_DARK_GRAY="\033[48;5;235m"
+TEXT_LIGHT_GRAY="\033[38;5;250m"
+TEXT_DARK_RED="\033[38;5;124m"
+TEXT_DARK_BLUE="\033[38;5;20m"
+TEXT_DARK_GREEN="\033[38;5;28m"
+
+BACKGROUND_LIGHT_GRAY="\033[48;5;245m"
 BACKGROUND_DARK_RED="\033[48;5;52m"
+BACKGROUND_DARK_BLUE="\033[48;5;19m"
+BACKGROUND_DARK_GREEN="\033[48;5;22m"
+
 RESET="\033[0m"
 
 is_error=0
 # Handle different events
 case "$event_name" in
   push)
-    output="● Detail"
-    output+="\n  - Event : ${TEXT_GREEN}Push${RESET}"
-    output+="\n  - Ref: $ref"
+    output="${TEXT_LIGHT_GRAY}● Detail${RESET}"
+    output+="${TEXT_LIGHT_GRAY}\n  - Event : ${TEXT_DARK_GREEN}Push${RESET}"
+    output+="${TEXT_LIGHT_GRAY}\n  - Ref: $ref${RESET}"
     if [[ "$ref" == refs/heads/* ]]; then
       branch_name=${ref#refs/heads/}
-      output+="\n  - Branch: ${TEXT_BLUE}$branch_name${RESET}"
-      summary="${BACKGROUND_DARK_GRAY}${TEXT_BLACK}Push Branch : $branch_name${RESET}\n"
+      output+="${TEXT_LIGHT_GRAY}\n  - Branch: ${TEXT_DARK_BLUE}$branch_name${RESET}"
+      summary="${BACKGROUND_DARK_GRAY}${TEXT_GREEN}Push Branch : $branch_name${RESET}\n"
     elif [[ "$ref" == refs/tags/* ]]; then
       tag_name=${ref#refs/tags/}
-      output+="\n  - Branch: ${TEXT_BLUE}$tag_name${RESET}"
-      summary="${BACKGROUND_DARK_GRAY}${TEXT_BLACK}Push Tag : $tag_name${RESET}\n"
+      output+="${TEXT_LIGHT_GRAY}\n  - Branch: ${TEXT_DARK_BLUE}$tag_name${RESET}"
+      summary="${BACKGROUND_DARK_GRAY}${TEXT_GREEN}Push Tag : $tag_name${RESET}\n"
     else
-      output+="\n  - ${TEXT_RED}Unknown${RESET}"
-      summary="${BACKGROUND_DARK_RED}${TEXT_BLACK}Push Unknown${RESET}\n"
+      output+="${TEXT_LIGHT_GRAY}\n  - ${TEXT_DARK_RED}Unknown${RESET}"
+      summary="${BACKGROUND_DARK_RED}${TEXT_RED}Push Unknown${RESET}\n"
       is_error=1
     fi
     ;;
   pull_request)
-    summary="${TEXT_GREEN}Pull Request${RESET}"
-    output="● Detail"
-    output+="\n  - Event : ${TEXT_GREEN}Pull Request${RESET}\n"
+    summary="${TEXT_GREEN}Pull Request"
+    output="${TEXT_LIGHT_GRAY}● Detail${RESET}"
+    output+="${TEXT_LIGHT_GRAY}\n  - Event : ${TEXT_DARK_GREEN}Pull Request${RESET}\n"
     if [[ "$head_ref" == refs/heads/* ]]; then
-      output+="\n  - Head Ref : ${TEXT_RED}$head_ref${RESET}"
-      summary+="\n  ${BACKGROUND_DARK_RED}${TEXT_BLACK}Invalid Head Ref ($head_ref) ${RESET}"
+      output+="${TEXT_LIGHT_GRAY}\n  - Head Ref : ${TEXT_DARK_RED}$head_ref${RESET}"
+      summary+="$\n  ${TEXT_RED}Invalid Head Ref ($head_ref)"
       is_error=1
     else
-      output+="\n  - Head Ref : $head_ref"
+      output+="${TEXT_LIGHT_GRAY}\n  - Head Ref : $head_ref${RESET}"
     fi
     if [[ "$base_ref" == refs/heads/* ]]; then
-      output+="\n  - Base Ref : ${TEXT_RED}$base_ref${RESET}"
-      summary+="\n  ${BACKGROUND_DARK_RED}${TEXT_BLACK}Invalid Base Ref ($base_ref) ${RESET}"
+      output+="${TEXT_LIGHT_GRAY}\n  - Base Ref : ${TEXT_DARK_RED}$base_ref${RESET}"
+      summary+="\n  ${TEXT_RED}Invalid Base Ref ($base_ref)"
       is_error=1
     else
-      output+="\n  - Base Ref : $base_ref"
+      output+="$\n  - Base Ref : $base_ref${RESET}"
     fi
     
     if [[ "$is_error" == "0" ]]; then
       head_branch_name=${head_ref#refs/heads/}
       base_branch_name=${base_ref#refs/heads/}
-      output+="\n  - Head Branch : $head_branch_name"
-      output+="\n  - Base Branch : $base_branch_name"
-      output+="\n  $head_branch_name -> $base_branch_name"
-      summary+=" : $head_branch_name -> $base_branch_name"
+      output+="${TEXT_LIGHT_GRAY}\n  - Head Branch : $head_branch_name${RESET}"
+      output+="${TEXT_LIGHT_GRAY}\n  - Base Branch : $base_branch_name${RESET}"
+      output+="${TEXT_LIGHT_GRAY}\n  $head_branch_name -> $base_branch_name${RESET}"
+      summary="${BACKGROUND_DARK_GRAY}${summary}${TEXT_WHITE} : $head_branch_name -> $base_branch_name${RESET}"
+    else
+      summary="${BACKGROUND_DARK_RED}${summary}${RESET}"
     fi
     summary+="\n"
     ;;
   *)
-    output="Event : ${TEXT_RED}$event_name${RESET}\n"
-    output+="${TEXT_RED}Unknown Event Type${RESET}"
-    summary="${BACKGROUND_DARK_RED}${TEXT_BLACK}Unknown Event Type ($base_ref) ${RESET}"
+    output=" ${TEXT_LIGHT_GRAY}Event : ${TEXT_DARK_RED}$event_name${RESET}\n"
+    output+="${TEXT_DARK_RED}Unknown Event Type${RESET}"
+    summary="${BACKGROUND_DARK_RED}${TEXT_RED}Unknown Event Type ($base_ref) ${RESET}"
     is_error=1
     ;;
 esac
